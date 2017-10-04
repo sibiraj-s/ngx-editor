a02205a8e40686476d47cae2796559066e1f3ae4
Sibiraj <sibiraj_s@live.com>
2017-10-04 18:43:29 +0530

    test: fix e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('ngx-editor App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('WYSIWYG Editor for Angular Applications.');
   });
 });
