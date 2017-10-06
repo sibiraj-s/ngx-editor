c81174701b679a35a10127c31f5f61d8d2b1d47c
Sibiraj <sibiraj_s@live.com>
2017-10-06 21:07:41 +0530

    chore: add ngx-resizer for resizing editor

--- src/app/ngx-editor/ngx-editor.module.ts
+++ src/app/ngx-editor/ngx-editor.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxEditorComponent } from './ngx-editor.component';
+import { NgxResizerModule } from 'ngx-resizer';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgxResizerModule
   ],
   declarations: [
     NgxEditorComponent
