0386c15ab3d2caf4245195f420f0dee5d17075a4
Sibiraj <sibiraj_s@live.com>
2017-11-22 14:45:20 +0530

    chore: cleanup

--- src/app/ngx-editor/common/utils/ngx-editor.utils.ts
+++ src/app/ngx-editor/common/utils/ngx-editor.utils.ts
@@ -1,7 +1,5 @@
-/*
- * enable or diable toolbar based on configuration
- */
-export function canEnableToolbarOptions(value, toolbar) {
+// enable or disable toolbar based on configuration
+export function canEnableToolbarOptions(value: string, toolbar: any): boolean {
 
     if (value) {
         if (toolbar['length'] === 0) {
@@ -16,3 +14,26 @@ export function canEnableToolbarOptions(value, toolbar) {
     }
 }
 
+// set editor configuration
+export function getEditorConfiguration(value, ngxEditorConfig): any {
+
+    for (const i in ngxEditorConfig) {
+        if (i) {
+            if (this[i]) {
+                value[i] = this[i];
+            }
+            if (!value.hasOwnProperty(i)) {
+                value[i] = ngxEditorConfig[i];
+            }
+        }
+    }
+    return value;
+}
+
+// return vertical if the element is the resizer property is set to basic
+export function canResize(resizer: string) {
+    if (resizer === 'basic') {
+        return 'vertical';
+    }
+    return false;
+}
