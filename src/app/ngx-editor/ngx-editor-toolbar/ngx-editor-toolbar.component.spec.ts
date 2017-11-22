8c7f6f9eeed357c23ce6d7a03c04e86b76c5b568
Sibiraj <sibiraj_s@live.com>
2017-11-22 11:45:00 +0530

    chore: rename ngx-toolbar to ngx-editor-toolbar

---
+++ src/app/ngx-editor/ngx-editor-toolbar/ngx-editor-toolbar.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NgxEditorToolbarComponent } from './ngx-editor-toolbar.component';
+import { ngxEditorConfig } from '../ngx-editor.defaults';
+
+describe('NgxEditorToolbarComponent', () => {
+  let component: NgxEditorToolbarComponent;
+  let fixture: ComponentFixture<NgxEditorToolbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [NgxEditorToolbarComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NgxEditorToolbarComponent);
+    component = fixture.componentInstance;
+    component.config = ngxEditorConfig;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/ngx-editor/ngx-editor.component.spec.ts
+++ src/app/ngx-editor/ngx-editor.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgxEditorComponent } from './ngx-editor.component';
 import { NgxGrippieComponent } from './ngx-grippie/ngx-grippie.component';
-import { NgxToolbarComponent } from './ngx-toolbar/ngx-toolbar.component';
+import { NgxEditorToolbarComponent } from './ngx-editor-toolbar/ngx-editor-toolbar.component';
 import { NgxEditorMessageComponent } from './ngx-editor-message/ngx-editor-message.component';
 import { MessageService } from './common/services/message.service';
 import { CommandExecutorService } from './common/services/command-executor.service';
@@ -16,7 +16,7 @@ describe('NgxEditorComponent', () => {
       providers: [MessageService, CommandExecutorService],
       declarations: [NgxEditorComponent,
         NgxGrippieComponent,
-        NgxToolbarComponent,
+        NgxEditorToolbarComponent,
         NgxEditorMessageComponent
       ]
     })
