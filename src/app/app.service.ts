07225ff064c192d5448483ae7461f35590f4fd68
Sibiraj <sibiraj_s@live.com>
2017-11-28 01:11:54 +0530

    chore: remove deprecated import

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -21,6 +20,5 @@ export class AppService {
         }
       });
   }
-
 
 }
