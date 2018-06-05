b074da2a3dd09c446259d1f053bd1ceffe4ef9bf
박영호 <mellonia.bluerose@gmail.com>
2018-06-06 00:23:02 +0900

    change font-size, font-color system

--- src/app/ngx-editor/ngx-editor-toolbar/ngx-editor-toolbar.component.ts
+++ src/app/ngx-editor/ngx-editor-toolbar/ngx-editor-toolbar.component.ts
@@ -37,7 +37,21 @@ export class NgxEditorToolbarComponent implements OnInit {
   hexColor = '';
   /** show/hide image uploader */
   isImageUploader = false;
-
+  /** font color list */
+  colors = [
+    ['#000000', '#424242', '#636363', '#9C9C94', '#CEC6CE', '#EFEFEF', '#F7F7F7', '#FFFFFF'],
+    ['#FF0000', '#FF9C00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#9C00FF', '#FF00FF'],
+    ['#F7C6CE', '#FFE7CE', '#FFEFC6', '#D6EFD6', '#CEDEE7', '#CEE7F7', '#D6D6E7', '#E7D6DE'],
+    ['#E79C9C', '#FFC69C', '#FFE79C', '#B5D6A5', '#A5C6CE', '#9CC6EF', '#B5A5D6', '#D6A5BD'],
+    ['#E76363', '#F7AD6B', '#FFD663', '#94BD7B', '#73A5AD', '#6BADDE', '#8C7BC6', '#C67BA5'],
+    ['#CE0000', '#E79439', '#EFC631', '#6BA54A', '#4A7B8C', '#3984C6', '#634AA5', '#A54A7B'],
+    ['#9C0000', '#B56308', '#BD9400', '#397B21', '#104A5A', '#085294', '#311873', '#731842'],
+    ['#630000', '#7B3900', '#846300', '#295218', '#083139', '#003163', '#21104A', '#4A1031']
+  ];
+  /** font size list */
+  sizes = [
+    '10px', '12px', '14px', '16px', '18px', '20px', '24px', '36px', '40px'
+  ];
   /**
    * Editor configuration
    */
@@ -257,6 +271,31 @@ export class NgxEditorToolbarComponent implements OnInit {
    */
   onlyNumbers(event: KeyboardEvent): boolean {
     return event.charCode >= 48 && event.charCode <= 57;
+  }
+
+  /**
+   * add style
+   * @param i index of first *ngFor
+   * @param j index of second *ngFor
+   */
+  getColor(i, j): any {
+    const style = {
+      'color': this.colors[i][j],
+      'cursor': 'pointer'
+    };
+    return style;
+  }
+
+  /**
+   * add style
+   * @param size font size
+   */
+  getSize(size): any {
+    const style = {
+      'font-size': size,
+      'cursor': 'pointer'
+    };
+    return style;
   }
 
   ngOnInit() {
