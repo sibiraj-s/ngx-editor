fbba60a6304be80dc77b0b4f5c478c60cfabcf29
sibiraj-s <dev.sibiraj@outlook.com>
2020-04-21 00:04:44 +0530

    test: fix editor not destoryed correctly

--- src/app/ngx-editor/ngx-editor.module.ts
+++ src/app/ngx-editor/ngx-editor.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { NgxEditorComponent } from './ngx-editor.component';
 
 @NgModule({
-  imports: [],
   declarations: [NgxEditorComponent],
   exports: [NgxEditorComponent],
 })
