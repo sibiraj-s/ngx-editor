e9b17613ea2a0f1575294011addf9fd77cea1d1a
sibiraj-s <dev.sibiraj@outlook.com>
2020-05-31 20:44:15 +0530

    chore: add sample  doc

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { environment } from '../environments/environment';
 
+import jsonDoc from './doc';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -11,10 +13,7 @@ import { environment } from '../environments/environment';
 export class AppComponent {
   isProdMode = environment.production;
 
-  editorContent: object = {
-    type: 'doc',
-    content: []
-  };
+  editorContent: object = jsonDoc;
 
   editorContentChange(doc: object) {
     this.editorContent = doc;
