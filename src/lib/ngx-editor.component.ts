587e288b6c794dead391d2f7cff02eca93a83a5f
sibiraj-s <dev.sibiraj@outlook.com>
2020-06-13 07:52:43 +0530

    fix: reactive forms

--- src/lib/ngx-editor.component.ts
+++ src/lib/ngx-editor.component.ts
@@ -1,6 +1,6 @@
 import {
   Component, ViewChild, ElementRef,
-  forwardRef, OnDestroy, ViewEncapsulation, AfterViewInit
+  forwardRef, OnDestroy, ViewEncapsulation, OnInit
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
@@ -22,7 +22,7 @@ import { NgxEditorService, NgxEditorServiceConfig } from './ngx-editor.service';
   encapsulation: ViewEncapsulation.None
 })
 
-export class NgxEditorComponent implements ControlValueAccessor, AfterViewInit, OnDestroy {
+export class NgxEditorComponent implements ControlValueAccessor, OnInit, OnDestroy {
   @ViewChild('ngxEditor', { static: true }) ngxEditor: ElementRef;
 
   private view: EditorView;
@@ -82,7 +82,7 @@ export class NgxEditorComponent implements ControlValueAccessor, AfterViewInit, 
 
     this.view.updateState(state);
 
-    if (tr.docChanged) {
+    if (tr.docChanged && this.onChange) {
       const json = state.doc.toJSON();
       this.onChange(json);
     }
@@ -106,7 +106,7 @@ export class NgxEditorComponent implements ControlValueAccessor, AfterViewInit, 
     this.editorInitialized = true;
   }
 
-  ngAfterViewInit() {
+  ngOnInit() {
     this.createEditor();
   }
 
