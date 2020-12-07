f1530b24b7db5d7dccb6708c2d0b9e9e7ae6b956
sibiraj-s <dev.sibiraj@outlook.com>
2020-12-07 20:55:31 +0530

    chore: update filename

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,6 +2,6 @@ export * from './lib/editor.component';
 export * from './lib/editor.module';
 
 export * from './lib/schema';
-export * from './lib/Validators';
+export * from './lib/validators';
 
 export * from './lib/types';
