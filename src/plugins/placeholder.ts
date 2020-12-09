ef4879ab42d9aa310cab8c42bc86b9852890baa2
sibiraj-s <dev.sibiraj@outlook.com>
2020-12-09 10:50:45 +0530

    feat: allow placeholder to be set via component

--- src/plugins/placeholder.ts
+++ src/plugins/placeholder.ts
@@ -1,4 +1,4 @@
-import { Plugin, EditorState, PluginKey } from 'prosemirror-state';
+import { Plugin, EditorState, PluginKey, Transaction } from 'prosemirror-state';
 import { DecorationSet, Decoration } from 'prosemirror-view';
 
 const DEFAULT_PLACEHOLDER = 'Type Here...';
@@ -7,14 +7,25 @@ const PLACEHOLDER_CLASSNAME = 'NgxEditor__Placeholder';
 const placeholderPlugin = (text: string = DEFAULT_PLACEHOLDER): Plugin => {
   return new Plugin({
     key: new PluginKey('placeholder'),
+    state: {
+      init(): string {
+        return text;
+      },
+      apply(tr: Transaction, previousVal: string): string {
+        const placeholder = tr.getMeta('UPDATE_PLACEHOLDER') ?? previousVal;
+        return placeholder;
+      }
+    },
     props: {
       decorations(state: EditorState): DecorationSet {
         const doc = state.doc;
 
+        const placeholder = this.getState(state);
+
         if (doc.childCount === 1 && doc?.firstChild?.isTextblock && doc.firstChild.content.size === 0) {
           const placeHolderEl = document.createElement('span');
           placeHolderEl.classList.add(PLACEHOLDER_CLASSNAME);
-          placeHolderEl.textContent = text;
+          placeHolderEl.textContent = placeholder;
           return DecorationSet.create(doc, [Decoration.widget(1, placeHolderEl)]);
         }
 
