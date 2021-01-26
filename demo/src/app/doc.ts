c91fb4a6c2c3f08fd45c7f171004b303680f4ba9
sibiraj-s <dev.sibiraj@outlook.com>
2021-01-26 11:54:43 +0530

    chore: setup arrow handlers for codemirror

--- demo/src/app/doc.ts
+++ demo/src/app/doc.ts
@@ -22,23 +22,7 @@ export default {
       content: [
         {
           type: 'text',
-          text: 'This is editable text. '
-        },
-        {
-          type: 'text',
-          marks: [
-            {
-              type: 'text_color',
-              attrs: {
-                color: '#d93f0b'
-              }
-            }
-          ],
-          text: 'You can focus it and start typing'
-        },
-        {
-          type: 'text',
-          text: '.'
+          text: 'This is editable text. You can focus it and start typing.'
         }
       ]
     },
@@ -50,12 +34,90 @@ export default {
       content: [
         {
           type: 'text',
+          text: 'To apply styling, you can select a piece of text and manipulate its styling from the menu. The basic schema supports '
+        },
+        {
+          type: 'text',
+          marks: [
+            {
+              type: 'em'
+            }
+          ],
+          text: 'emphasis'
+        },
+        {
+          type: 'text',
+          text: ', '
+        },
+        {
+          type: 'text',
+          marks: [
+            {
+              type: 'strong'
+            }
+          ],
+          text: 'strong text'
+        },
+        {
+          type: 'text',
+          text: ', '
+        },
+        {
+          type: 'text',
+          marks: [
+            {
+              type: 'u'
+            }
+          ],
+          text: 'underline'
+        },
+        {
+          type: 'text',
+          text: ', '
+        },
+        {
+          type: 'text',
+          marks: [
+            {
+              type: 's'
+            }
+          ],
+          text: 'strike'
+        },
+        {
+          type: 'text',
+          text: ', '
+        },
+        {
+          type: 'text',
+          marks: [
+            {
+              type: 'link',
+              attrs: {
+                href: 'http://marijnhaverbeke.nl/blog',
+                title: null,
+                target: null
+              }
+            }
+          ],
+          text: 'links'
+        },
+        {
+          type: 'text',
+          text: ', '
+        },
+        {
+          type: 'text',
           marks: [
             {
               type: 'code'
             }
           ],
-          text: 'code block'
+          text: 'code font'
+        },
+        {
+          type: 'text',
+          text: '.'
         }
       ]
     },
@@ -75,11 +137,11 @@ export default {
                   type: 'strong'
                 }
               ],
-              text: 'Lorem Ipsum'
+              text: 'Blockquote: '
             },
             {
               type: 'text',
-              text: ' is '
+              text: 'Lorem Ipsum is '
             },
             {
               type: 'text',
@@ -193,367 +255,6 @@ export default {
         {
           type: 'text',
           text: 'The content of the code editor is kept in sync with the content of the code block in the rich text editor, so that it is as if you\'re directly editing the outer document, using a more convenient interface.'
-        }
-      ]
-    },
-    {
-      type: 'heading',
-      attrs: {
-        level: 4,
-        align: 'center'
-      },
-      content: [
-        {
-          type: 'text',
-          text: 'Mr. Bean'
-        }
-      ]
-    },
-    {
-      type: 'paragraph',
-      attrs: {
-        align: 'center'
-      },
-      content: [
-        {
-          type: 'text',
-          text: 'The image is resizable. Include '
-        },
-        {
-          type: 'text',
-          marks: [
-            {
-              type: 'strong'
-            }
-          ],
-          text: 'image'
-        },
-        {
-          type: 'text',
-          text: ' plugin to enable image resizing'
-        }
-      ]
-    },
-    {
-      type: 'heading',
-      attrs: {
-        level: 3,
-        align: 'center'
-      },
-      content: [
-        {
-          type: 'image',
-          attrs: {
-            src: 'https://wallpapercave.com/wp/wp2318909.png',
-            alt: 'Bean',
-            title: 'Mr. Bean',
-            width: '98px'
-          }
-        }
-      ]
-    },
-    {
-      type: 'heading',
-      attrs: {
-        level: 3,
-        align: null
-      },
-      content: [
-        {
-          type: 'text',
-          text: 'Bullet list'
-        }
-      ]
-    },
-    {
-      type: 'bullet_list',
-      content: [
-        {
-          type: 'list_item',
-          content: [
-            {
-              type: 'paragraph',
-              attrs: {
-                align: null
-              },
-              content: [
-                {
-                  type: 'text',
-                  marks: [
-                    {
-                      type: 'strong'
-                    }
-                  ],
-                  text: 'Lorem Ipsum'
-                },
-                {
-                  type: 'text',
-                  text: ' is simply dummy text of the printing and typesetting industry'
-                }
-              ]
-            },
-            {
-              type: 'bullet_list',
-              content: [
-                {
-                  type: 'list_item',
-                  content: [
-                    {
-                      type: 'paragraph',
-                      attrs: {
-                        align: null
-                      },
-                      content: [
-                        {
-                          type: 'text',
-                          text: '('
-                        },
-                        {
-                          type: 'text',
-                          marks: [
-                            {
-                              type: 'strong'
-                            }
-                          ],
-                          text: 'depth 1'
-                        },
-                        {
-                          type: 'text',
-                          text: ') It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
-                        }
-                      ]
-                    },
-                    {
-                      type: 'bullet_list',
-                      content: [
-                        {
-                          type: 'list_item',
-                          content: [
-                            {
-                              type: 'paragraph',
-                              attrs: {
-                                align: null
-                              },
-                              content: [
-                                {
-                                  type: 'text',
-                                  text: '('
-                                },
-                                {
-                                  type: 'text',
-                                  marks: [
-                                    {
-                                      type: 'strong'
-                                    }
-                                  ],
-                                  text: 'depth 2'
-                                },
-                                {
-                                  type: 'text',
-                                  text: ') The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.'
-                                }
-                              ]
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        {
-          type: 'list_item',
-          content: [
-            {
-              type: 'paragraph',
-              attrs: {
-                align: null
-              },
-              content: [
-                {
-                  type: 'text',
-                  text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          type: 'list_item',
-          content: [
-            {
-              type: 'paragraph',
-              attrs: {
-                align: null
-              },
-              content: [
-                {
-                  type: 'text',
-                  text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    },
-    {
-      type: 'heading',
-      attrs: {
-        level: 4,
-        align: null
-      },
-      content: [
-        {
-          type: 'text',
-          text: 'Ordered List'
-        }
-      ]
-    },
-    {
-      type: 'ordered_list',
-      attrs: {
-        order: 1
-      },
-      content: [
-        {
-          type: 'list_item',
-          content: [
-            {
-              type: 'paragraph',
-              attrs: {
-                align: null
-              },
-              content: [
-                {
-                  type: 'text',
-                  marks: [
-                    {
-                      type: 'strong'
-                    }
-                  ],
-                  text: 'Lorem Ipsum'
-                },
-                {
-                  type: 'text',
-                  text: ' is simply dummy text of the printing and typesetting industry'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          type: 'list_item',
-          content: [
-            {
-              type: 'paragraph',
-              attrs: {
-                align: null
-              },
-              content: [
-                {
-                  type: 'text',
-                  text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable'
-                }
-              ]
-            },
-            {
-              type: 'ordered_list',
-              attrs: {
-                order: 1
-              },
-              content: [
-                {
-                  type: 'list_item',
-                  content: [
-                    {
-                      type: 'paragraph',
-                      attrs: {
-                        align: null
-                      },
-                      content: [
-                        {
-                          type: 'text',
-                          text: '('
-                        },
-                        {
-                          type: 'text',
-                          marks: [
-                            {
-                              type: 'strong'
-                            }
-                          ],
-                          text: 'depth 1'
-                        },
-                        {
-                          type: 'text',
-                          text: ') It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
-                        }
-                      ]
-                    },
-                    {
-                      type: 'ordered_list',
-                      attrs: {
-                        order: 1
-                      },
-                      content: [
-                        {
-                          type: 'list_item',
-                          content: [
-                            {
-                              type: 'paragraph',
-                              attrs: {
-                                align: null
-                              },
-                              content: [
-                                {
-                                  type: 'text',
-                                  text: '('
-                                },
-                                {
-                                  type: 'text',
-                                  marks: [
-                                    {
-                                      type: 'strong'
-                                    }
-                                  ],
-                                  text: 'depth 2'
-                                },
-                                {
-                                  type: 'text',
-                                  text: ') The chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.'
-                                }
-                              ]
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        {
-          type: 'list_item',
-          content: [
-            {
-              type: 'paragraph',
-              attrs: {
-                align: null
-              },
-              content: [
-                {
-                  type: 'text',
-                  text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
-                }
-              ]
-            }
-          ]
         }
       ]
     }
--- demo/src/app/plugins/index.ts
+++ demo/src/app/plugins/index.ts
@@ -1,11 +1,43 @@
-import { Plugin } from 'prosemirror-state';
+import { EditorState, Plugin, Selection, Transaction } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import { keymap } from 'prosemirror-keymap';
+
 import { image } from 'ngx-editor/plugins';
+import { Command } from 'prosemirror-commands';
+
+type Dir = 'left' | 'right' | 'up' | 'down';
+
+const arrowHandler = (dir: Dir): Command => {
+  return (state: EditorState, dispatch: (tr: Transaction) => void, view: EditorView): boolean => {
+    if (state.selection.empty && view.endOfTextblock(dir)) {
+      const { doc } = state;
+      const { $head } = state.selection;
+
+      const side = dir === 'left' || dir === 'up' ? -1 : 1;
+      const nextPos = Selection.near(doc.resolve(side > 0 ? $head.after() : $head.before()), side);
+
+      if (nextPos.$head && nextPos.$head.parent.type.name === 'code_mirror') {
+        dispatch(state.tr.setSelection(nextPos));
+        return true;
+      }
+    }
+    return false;
+  };
+};
+
+const arrowHandlers = keymap({
+  ArrowLeft: arrowHandler('left'),
+  ArrowRight: arrowHandler('right'),
+  ArrowUp: arrowHandler('up'),
+  ArrowDown: arrowHandler('down')
+});
 
 const getPlugins = (): Plugin[] => {
   const plugins = [
     image({
       resize: true,
-    })
+    }),
+    arrowHandlers
   ];
 
   return plugins;
