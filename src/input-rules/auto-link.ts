c18d1ede650e8d7682b6a4da6acc60e381b3e667
sibiraj-s <dev.sibiraj@outlook.com>
2021-02-13 07:58:34 +0530

    feat: add auto-link input rule

---
+++ src/input-rules/auto-link.ts
@@ -0,0 +1,15 @@
+import { InputRule } from 'prosemirror-inputrules';
+
+// automatically convert text to link while typing
+export const autoLink = (): InputRule => {
+  const urlRegEx = /((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)$/;
+
+  return new InputRule(urlRegEx, (state, match, start, end) => {
+    const { schema } = state;
+
+    const tr = state.tr.insertText(match[0], start, end); // Replace existing text with entire match
+    const mark = schema.marks.link.create({ href: match[0] });
+
+    return tr.addMark(start, start + match[0].length, mark);
+  });
+};
