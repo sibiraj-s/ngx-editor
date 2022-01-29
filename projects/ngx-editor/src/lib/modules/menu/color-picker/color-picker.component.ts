9089c5a30cb549a23d81943883af1a7b3b14f3e3
Sibiraj <dev.sibiraj@outlook.com>
2022-01-29 19:12:13 +0530

    fix: replace deprecated substr with substring

--- projects/ngx-editor/src/lib/modules/menu/color-picker/color-picker.component.ts
+++ projects/ngx-editor/src/lib/modules/menu/color-picker/color-picker.component.ts
@@ -55,10 +55,10 @@ export class ColorPickerComponent implements OnInit, OnDestroy {
   private canExecute = true;
 
   getContrastYIQ(hexcolor: string): string {
-    hexcolor = hexcolor.replace('#', '');
-    const r = parseInt(hexcolor.substr(0, 2), 16);
-    const g = parseInt(hexcolor.substr(2, 2), 16);
-    const b = parseInt(hexcolor.substr(4, 2), 16);
+    const color = hexcolor.replace('#', '');
+    const r = parseInt(color.substring(0, 2), 16);
+    const g = parseInt(color.substring(2, 4), 16);
+    const b = parseInt(color.substring(4, 6), 16);
     const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
     return (yiq >= 128) ? 'black' : 'white';
   }
