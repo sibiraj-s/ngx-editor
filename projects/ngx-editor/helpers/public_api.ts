1d9b4d1d7214bb30e72c92dc38b4766eefe20079
Sibiraj <dev.sibiraj@outlook.com>
2022-02-20 00:32:59 +0530

    chore: remove unused helpers

--- projects/ngx-editor/helpers/public_api.ts
+++ projects/ngx-editor/helpers/public_api.ts
@@ -2,7 +2,6 @@ export * from './isMarkActive';
 export * from './isLinkActive';
 export * from './isNodeActive';
 export * from './getSelectionMarks';
-export * from './bubblePosition';
 export * from './getSelectionNodes';
 export * from './markApplies';
 export * from './markInputRule';
