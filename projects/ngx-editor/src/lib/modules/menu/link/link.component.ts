a5c92cc3bf40db0a2d0fe49968930840caa1feb4
Alex <avicente@watchity.com>
2022-05-03 12:46:12 +0200

    fix: more validator improvements and add https:// on click

--- projects/ngx-editor/src/lib/modules/menu/link/link.component.ts
+++ projects/ngx-editor/src/lib/modules/menu/link/link.component.ts
@@ -33,10 +33,10 @@ export class LinkComponent implements OnInit, OnDestroy {
   private updateSubscription: Subscription;
 
   form = new FormGroup({
-    href: new FormControl('', [
+    href: new FormControl('https://', [
       Validators.required,
       Validators.pattern(
-        '((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[\\w]*))?)',
+        '((([A-Za-z]{3,9}:(?:\\/\\/))(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[\\w]*))?)',
       ),
     ]),
     text: new FormControl('', [Validators.required]),
