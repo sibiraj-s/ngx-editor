517ea711ae15e2f670c2734d784a2da2b9afa89d
Schmailzl, Sebastian <sebastian.schmailzl@wk-it.com>
2022-07-21 09:57:49 +0200

    feat: added mailto support to Link Component

--- projects/ngx-editor/src/lib/modules/menu/link/link.component.ts
+++ projects/ngx-editor/src/lib/modules/menu/link/link.component.ts
@@ -28,7 +28,7 @@ export class LinkComponent implements OnInit, OnDestroy {
   form = new FormGroup({
     href: new FormControl('', [
       Validators.required,
-      Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/??([^#\n\r]*)?#?([^\n\r]*)'),
+      Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/??([^#\n\r]*)?#?([^\n\r]*)|(mailto:.*[@].*)'),
     ]),
     text: new FormControl('', [Validators.required]),
     openInNewTab: new FormControl(true),
