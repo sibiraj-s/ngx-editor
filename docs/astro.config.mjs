b509fce6c3a643b8efe8f1d806c042c1dd9f74f3
Sibiraj <dev.sibiraj@outlook.com>
2022-08-16 10:54:44 +0530

    docs: fix build base URL

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,26 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
+const github = () => {
+  /** @type {import('astro').AstroIntegration}} */
+  const adapter = {
+    name: 'github',
+    hooks: {
+      'astro:config:setup': ({ command, updateConfig }) => {
+        if (command !== 'build') {
+          return
+        }
+
+        updateConfig({
+          base: '/ngx-editor/'
+        })
+      }
+    },
+  };
+
+  return adapter
+}
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -10,5 +30,10 @@ export default defineConfig({
     // Enable React for the Algolia search component.
     react(),
   ],
-  site: `https://sibiraj-s.github.io`
+  site: `https://sibiraj-s.github.io`,
+  outDir: './dist',
+  publicDir: './public',
+  srcDir: './src',
+  trailingSlash: 'ignore',
+  adapter: github()
 });
