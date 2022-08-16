f000fc9e9b377838d22c31277fa2dfb22e93b5b1
Sibiraj <dev.sibiraj@outlook.com>
2022-08-16 10:32:54 +0530

    docs: fix site URL

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,11 +4,11 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `http://astro.build`,
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+  ],
+  site: `https://sibiraj-s.github.io`
 });
