8c13e54b1fc111ed22ad00816877d1c61a8d2203
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-06-12 13:34:52 +0530

    docs: fix base url for development

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sibiraj-s.github.io',
-  base: '/ngx-editor',
+  base: process.env.NODE_ENV === 'produciton'? '/ngx-editor': '/',
   integrations: [
     starlight({
       title: 'NgxEditor',
