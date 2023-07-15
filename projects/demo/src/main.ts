4a9e7d0b10aadcd2f438ba2cad630c5c9460675b
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-07-15 13:08:14 +0530

    feat: upgrade to angular v16

--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -1,12 +1,8 @@
-import { enableProdMode } from '@angular/core';
+/* eslint-disable no-console */
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch((err) => console.error(err)); // eslint-disable-line no-console
+  .catch((err) => console.error(err));
