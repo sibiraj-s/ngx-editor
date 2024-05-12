c038772ed2ea497e8b6f8849833724b0f57ce8f8
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2024-05-12 17:43:12 +0530

    fix: unset link only when removed

--- projects/ngx-editor/commands/removeLink.ts
+++ projects/ngx-editor/commands/removeLink.ts
@@ -2,8 +2,10 @@ import { EditorState, Transaction, type Command } from 'prosemirror-state';
 
 export const removeLink = (): Command => {
   return (state: EditorState, dispatch?: (tr: Transaction) => void): boolean => {
-    const { doc, selection, tr } = state;
+    const { doc, selection, tr, schema } = state;
     const { $head: { pos }, from, to } = selection;
+
+    const linkMark = schema.marks['link'];
 
     // if the cursor is on the link without any selection
     if (from === to) {
@@ -11,9 +13,9 @@ export const removeLink = (): Command => {
       const linkStart = pos - $pos.textOffset;
       const linkEnd = linkStart + $pos.parent.child($pos.index()).nodeSize;
 
-      tr.removeMark(linkStart, linkEnd);
+      tr.removeMark(linkStart, linkEnd, linkMark);
     } else {
-      tr.removeMark(from, to);
+      tr.removeMark(from, to, linkMark);
     }
 
     if (!tr.docChanged) {
