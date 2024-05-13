ac2d4e74e6b46c11343f0b443c4068499673560e
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2024-05-13 20:23:10 +0530

    chore: improve mark typings

--- projects/ngx-editor/schema/marks.ts
+++ projects/ngx-editor/schema/marks.ts
@@ -1,4 +1,4 @@
-import { DOMOutputSpec, Mark, MarkSpec } from 'prosemirror-model';
+import { Mark, MarkSpec } from 'prosemirror-model';
 
 // :: MarkSpec A link. Has `href` and `title` attributes. `title`
 // defaults to the empty string. Rendered and parsed as an `<a>`
@@ -13,7 +13,7 @@ const link: MarkSpec = {
   parseDOM: [
     {
       tag: 'a[href]',
-      getAttrs(dom: HTMLElement): Record<string, any> {
+      getAttrs(dom) {
         return {
           href: dom.getAttribute('href'),
           title: dom.getAttribute('title'),
@@ -22,7 +22,7 @@ const link: MarkSpec = {
       },
     },
   ],
-  toDOM(node): DOMOutputSpec {
+  toDOM(node) {
     const { href, title, target } = node.attrs;
     return ['a', { href, title, target }, 0];
   },
@@ -36,7 +36,7 @@ const em: MarkSpec = {
     { tag: 'em' },
     { style: 'font-style=italic' },
   ],
-  toDOM(): DOMOutputSpec {
+  toDOM() {
     return ['em', 0];
   },
 };
@@ -62,7 +62,7 @@ const strong: MarkSpec = {
       },
     },
   ],
-  toDOM(): DOMOutputSpec {
+  toDOM() {
     return ['strong', 0];
   },
 };
@@ -72,7 +72,7 @@ const code: MarkSpec = {
   parseDOM: [
     { tag: 'code' },
   ],
-  toDOM(): DOMOutputSpec {
+  toDOM() {
     return ['code', 0];
   },
 };
@@ -87,7 +87,7 @@ const u: MarkSpec = {
       consuming: false,
     },
   ],
-  toDOM(): DOMOutputSpec {
+  toDOM() {
     return ['u', 0];
   },
 };
@@ -100,7 +100,7 @@ const s: MarkSpec = {
     { tag: 'strike' },
     { style: 'text-decoration=line-through' },
   ],
-  toDOM(): DOMOutputSpec {
+  toDOM() {
     return ['s', 0];
   },
 };
@@ -114,12 +114,12 @@ const textColor: MarkSpec = {
   parseDOM: [
     {
       style: 'color',
-      getAttrs: (value: string): Record<string, any> => {
+      getAttrs: (value: string) => {
         return { color: value };
       },
     },
   ],
-  toDOM(mark: Mark): DOMOutputSpec {
+  toDOM(mark: Mark) {
     const { color } = mark.attrs;
     return ['span', { style: `color:${color};` }, 0];
   },
@@ -134,12 +134,12 @@ const textBackgroundColor: MarkSpec = {
   parseDOM: [
     {
       style: 'background-color',
-      getAttrs: (value: string): Record<string, any> => {
+      getAttrs: (value: string) => {
         return { backgroundColor: value };
       },
     },
   ],
-  toDOM(mark: Mark): DOMOutputSpec {
+  toDOM(mark: Mark) {
     const { backgroundColor } = mark.attrs;
     return ['span', { style: `background-color:${backgroundColor};` }, 0];
   },
