6f4e4169b0bbd8904f4dc2df16b0e5aa4b9a280c
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2025-01-28 23:12:25 +0530

    chore: fix build scripts

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs/promises');
 const path = require('node:path');
-const chalk = require('chalk');
+const pc = require('picocolors');
 
 const copyFile = async function (srcFilePath, destFilePath) {
   const fileName = path.basename(srcFilePath);
@@ -8,9 +8,9 @@ const copyFile = async function (srcFilePath, destFilePath) {
     const srcPath = path.resolve(process.cwd(), srcFilePath);
     const destPath = path.resolve(process.cwd(), 'dist/ngx-editor', destFilePath);
     await fs.copyFile(srcPath, destPath);
-    console.log(chalk.green(`- File Copied: ${fileName}`));
+    console.log(pc.green(`- File Copied: ${fileName}`));
   } catch (err) {
-    console.log(chalk.red(`Error while copying ${fileName}`), err);
+    console.log(pc.red(`Error while copying ${fileName}`), err);
   }
 };
 
